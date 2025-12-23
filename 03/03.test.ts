57268b70869343c95f42f4a23e5cae48b9c904dc
Núria Soriano Satorres <nuria.soriano@voicemod.net>
2025-12-23 10:38:24 +0100

    day 3 part 2

--- 03/03.test.ts
+++ 03/03.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { part1 } from "./03";
+import { getVoltage, part1, part2 } from "./03";
 
 describe("Day 03", () => {
 	describe("part 1", () => {
@@ -13,6 +13,24 @@ describe("Day 03", () => {
 			const result = part1("03/input.txt");
 
 			expect(result).toBe(17196);
+		});
+	});
+	describe("part 2", () => {
+		it("should get voltage", () => {
+			const result = getVoltage("811111111111119");
+
+			expect(result).toBe("811111111119");
+		});
+		it("should solve part 2 with input", () => {
+			const result = part2("03/input-example.txt");
+
+			expect(result).toBe(3121910778619);
+		});
+
+		it("should solve part 2 with input", () => {
+			const result = part2("03/input.txt");
+
+			expect(result).toBe(171039099596062);
 		});
 	});
 });
--- 03/03.ts
+++ 03/03.ts
@@ -3,8 +3,6 @@ import { parseInput } from "../parseInput";
 export function part1(file: string): number {
 	const banks = parseInput(file);
 
-	console.log(banks);
-
 	const voltagePerBank = banks
 		.map((bank) => {
 			const numbers = bank.split("").map((digit) => parseInt(digit));
@@ -26,6 +24,37 @@ export function part1(file: string): number {
 				highestDigitIndex
 			].toString()}${remainingNumbersSorted[0].toString()}`;
 		})
+		.reduce((acc, curr) => acc + parseInt(curr), 0);
+
+	return voltagePerBank;
+}
+
+export function getVoltage(bank: string): string {
+	const numbers = bank.split("").map((digit) => parseInt(digit));
+	let voltage = "";
+	let remainingNumbers = [...numbers];
+
+	for (let i = 12; i > 0; i--) {
+		const slicedNumbers = remainingNumbers.slice(
+			0,
+			remainingNumbers.length - i + 1
+		);
+		const sortedDigits = [...slicedNumbers].sort((a, b) => b - a);
+		const highestDigitIndex = remainingNumbers.indexOf(sortedDigits[0]);
+
+		remainingNumbers = remainingNumbers.slice(highestDigitIndex + 1);
+
+		voltage = `${voltage}${sortedDigits[0].toString()}`;
+	}
+
+	return voltage;
+}
+
+export function part2(file: string): number {
+	const banks = parseInput(file);
+
+	const voltagePerBank = banks
+		.map(getVoltage)
 		.reduce((acc, curr) => acc + parseInt(curr), 0);
 
 	return voltagePerBank;
