0bea80f0efcfafd65ed56c96583027e703b2a9e3
Núria Soriano Satorres <nuria.soriano@voicemod.net>
2025-12-23 17:27:09 +0100

    day 4 part 2

--- 04/04.test.ts
+++ 04/04.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { part1 } from "./04";
+import { part1, part2 } from "./04";
 
 describe("Day 04", () => {
 	describe("part 1", () => {
@@ -13,6 +13,18 @@ describe("Day 04", () => {
 			const result = part1("04/input.txt");
 
 			expect(result).toBe(1493);
+		});
+	});
+	describe("part 2", () => {
+		it("should solve part 2 with example input", () => {
+			const result = part2("04/input-example.txt");
+
+			expect(result).toBe(43);
+		});
+		it("should solve part 2 with input", () => {
+			const result = part2("04/input.txt");
+
+			expect(result).toBe(9194);
 		});
 	});
 });
--- 04/04.ts
+++ 04/04.ts
@@ -26,3 +26,51 @@ export function part1(file: string): number {
 			return adjacentRolls < 4;
 		}).length;
 }
+
+export function part2(file: string): number {
+	const grid = parseInput(file).map((line) => line.split(""));
+	const directions = [
+		[-1, -1],
+		[-1, 0],
+		[-1, 1],
+		[0, -1],
+		[0, 1],
+		[1, -1],
+		[1, 0],
+		[1, 1],
+	];
+
+	const copyGrid = grid.map((row) => [...row]);
+
+	let pickedUpRolls = true;
+
+	let count = 0;
+
+	while (pickedUpRolls) {
+		const pickedRolls = copyGrid
+			.flatMap((row, i) => row.map((cell, j) => ({ cell, i, j })))
+			.filter(({ cell, i, j }) => {
+				if (cell !== "@") {
+					return false;
+				}
+				const adjacentRolls = directions.filter(
+					([di, dj]) => copyGrid[i + di]?.[j + dj] === "@"
+				).length;
+
+				const hasPickedRoll = adjacentRolls < 4;
+
+				if (hasPickedRoll) {
+					copyGrid[i][j] = ".";
+				}
+
+				return hasPickedRoll;
+			}).length;
+
+		count += pickedRolls;
+		if (pickedRolls === 0) {
+			pickedUpRolls = false;
+			break;
+		}
+	}
+	return count;
+}
